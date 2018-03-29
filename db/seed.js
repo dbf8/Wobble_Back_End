703cefba34801da3f8fd1186a3ca7d88dc5f9977
Daniel Freudberg <dbfreudber@gmail.com>
2018-03-29 10:45:05 -0400

    incorporated seed data, deleted a random stylesheet leftover from the skeleton

--- db/seed.js
+++ db/seed.js
@@ -2,12 +2,108 @@ const Website = require('../models/Websites')
 
 Website.remove({}).then(() => {
   Promise.all([
-    Website.create({
-      title: 'STUMBLEUPON',
-      website: 'www.stumbleupon.com', // too meta?
-      username: 'Admin',
-      rating: 0
-    })
+    Website.create(
+      {
+        title: '27 Protein Shake Recipes to Rock Your Routines',
+        website:
+          'http://infographicjournal.com/27-protein-shake-recipes-to-rock-your-routines/',
+        username: 'admin',
+        rating: 0
+      },
+      {
+        title: 'More Amazing Photos From The Past',
+        website:
+          'https://wildplatypus.com/2017/03/07/more-amazing-photos-from-the-past/',
+        username: 'admin',
+        rating: 0
+      },
+      {
+        title: '10 Essential Cheat Sheets To Download',
+        website: 'https://www.makeuseof.com/tag/7-essential-cheat-sheets/',
+        username: 'admin',
+        rating: 0
+      },
+      {
+        title: 'Watch Star Wars Episode IV in Your Command Prompt for Free',
+        website:
+          'http://mytechquest.com/windows/watch-star-wars-episode-iv-in-your-command-prompt-for-free/',
+        username: 'admin',
+        rating: 0
+      },
+      {
+        title: 'Secret Netflix Codes: The Complete List (2018 Update)',
+        website:
+          'https://www.momdeals.com/mom-hacks/these-secret-netflix-codes-can-unlock-thousands-of-hidden-categories-693',
+        username: 'admin',
+        rating: 0
+      },
+      {
+        title: '77 Drinks Every Gentleman Should Know how to make',
+        website:
+          'http://thebaumlist.com/77-drinks-every-gentleman-should-know-how-to-make/',
+        username: 'admin',
+        rating: 0
+      },
+      {
+        title: '13 OF THE SMARTEST QUESTIONS TO ASK A HIRING MANAGER',
+        website:
+          'https://www.topresume.com/career-advice/13-of-the-smartest-questions-to-ask-a-hiring-manager',
+        username: 'admin',
+        rating: 0
+      },
+      {
+        title: '100 Words to Make You Sound Smart',
+        website: 'https://www.vocabulary.com/lists/258109',
+        username: 'admin',
+        rating: 0
+      },
+      {
+        title: 'Cute Puppies',
+        website: 'http://www.astro.cornell.edu/~derg/puppies.html',
+        username: 'admin',
+        rating: 0
+      },
+      {
+        title: 'TOP 10 SMOOTHIES AND DRINKS THAT BOOST YOUR METABOLISM',
+        website:
+          'http://www.womenio.com/1880/top-10-smoothies-and-drinks-that-boost-your-metabolism',
+        username: '',
+        rating: 0
+      },
+      {
+        title: 'Do You Know What Your Favorite Foods Look Like While Growing?',
+        website:
+          'http://www.trueactivist.com/do-you-know-what-your-favorite-foods-look-like-while-growing/',
+        username: 'admin',
+        rating: 0
+      },
+      {
+        title: '100 questions to ask people',
+        website: 'http://marshalljonesjr.com/100-questions-to-ask-people/',
+        username: 'admin',
+        rating: 0
+      },
+      {
+        title: '23 Places to Find Odd and Interesting Gifts and Purchases',
+        website:
+          'http://johnnylists.com/23-places-to-find-odd-and-interesting-gifts-and/',
+        username: 'admin',
+        rating: 0
+      },
+      {
+        title: 'The Only 8 TED Talks You Need to Watch',
+        website: 'http://www.mydomaine.com/life-changing-ted-talks',
+        username: 'admin',
+        rating: 0
+      },
+      {
+        title: '50 Things You Must Do When in Paris',
+        website:
+          'http://thesundaychapter.com/2016/01/50-things-to-do-in-paris/',
+        username: 'admin',
+        rating: 0
+      }
+    )
   ]).then(() => {
     console.log('done')
     process.exit()
